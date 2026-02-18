0ff530120fbdb2969b3717d151c4ef42b7e47b5a
TmTanky <tmtanky@gmail.com>
2026-02-18 10:16:46 +0800

    feat: added seo and sitemap

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,20 @@ import { Footer } from "@/components/footer/Footer"
 
 export const metadata: Metadata = {
   title: "DPRDS | Software Engineer",
-  description: "Welcome to my portfolio website.",
+  description:
+    "I'm a curious software engineer striving to improve every day. I love making user-friendly web apps that everyone can enjoy. I love accessibility, ensuring my apps are easy for everyone to use.",
+  keywords: [
+    "paredes",
+    "dinmarc",
+    "dinmarc paredes",
+    "software engineer",
+    "frontend enginneer",
+    "web developer",
+    "frontend developer",
+    "software",
+    "frontend",
+    "dprds",
+  ],
 }
 
 export default function Home() {
